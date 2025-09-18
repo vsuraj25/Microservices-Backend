6129188bc1c90e5f958d8995788021090bad94d8
vsuraj25 <stdmail2552@gmail.com>
2025-09-18 20:26:51 +0530

    feat: updated product model

--- products/models.py
+++ products/models.py
@@ -4,3 +4,8 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
     likes = models.PositiveIntegerField(default=0)
+
+class User(models.Model):
+    pass
+
+
